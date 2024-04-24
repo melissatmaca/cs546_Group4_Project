8d0ce06f1da154232f404078e992b3b6d2ba12d7
cadecermak <ccermak@stevens.edu>
2024-04-24 14:09:58 -0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
 export const generateRandomString = () => {
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-
-  for (let i = 0; i < 16; i++) {
-      const randomIndex = Math.floor(Math.random() * letters.length);
-      result = result + letters.charAt(randomIndex);
-    }
-
-  return result;
-};
+      const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let result = '';
+  
+      for (let i = 0; i < 16; i++) {
+          const randomIndex = Math.floor(Math.random() * letters.length);
+          result = result + letters.charAt(randomIndex);
+        }
+  
+      return result;
+  };
