65de67458880f4523aa604cf7c56ca3346db2e3a
melissatmaca <melissaatmaca@outlook.com>
2024-04-30 19:28:22 -0400

    Added routes and created data functions.

---
+++ data/users.js
@@ -0,0 +1,55 @@
+import bcrypt from 'bcrypt';
+import {users} from '../config/mongoCollections.js';
+import * as helper from '../validation.js';
+
+
+export const createUser = async(firstName, lastName, email, username, password) => {
+    if(!firstName, !lastName, !email, !username, !password) throw `Please complete al fields.`;
+    firstName = helper.checkString(firstName, "First name");
+    lastName = helper.checkString(lastName, "Last name");
+    email = helper.checkEmail(email);
+    username = helper.checkUsername(username);
+    password = helper.checkPassword(password);
+
+    let hashedPassword = bcrypt.hash(password, 16);
+
+    const userCollection = await users();
+
+    const usernameExists = await userCollection.findOne({username: username.toLowerCase()});
+    if(usernameExists) throw `The username already exists.`;
+
+    let newUser = {
+        firstName : firstName,
+        lastName : lastName, 
+        email : email, 
+        username : username, 
+        password : hashedPassword
+    }
+
+    const newUserInfo = await userCollection.insertOne(newUser);
+    if(!newUserInfo.insertedId || !newUserInfo.acknowledged) throw `Account cannot be created.`;
+
+    return {signupCompleted: true};
+};
+
+export const loginUser = async(username, password) => {
+    if(!username) throw `Please provide a username`;
+    if(!password) throw `Please provide a password`;
+;
+    username = helper.checkUsername(username);
+    password = helper.checkPassword(password);
+
+    let userCollection = await users();
+
+    const usernameExists = await userCollection.findOne({username: username.toLowerCase()});
+    if(!usernameExists) throw `Either the username or password is inhelper.`;
+
+    let passwordMatches = await bcrypt.compare(password, usernameExists.password);
+    if(!passwordMatches) throw `Either the username or password is invalid`;
+
+    let userInf = await userCollection.find({username: username.toLowerCase()});
+
+    if(!userInf) throw `Account cannot be located.`;
+
+    return {loginCompleted:true};
+};
---
+++ routes/users.js
@@ -0,0 +1,45 @@
+import * as helper from '../helpers.js';
+import {createUser, loginUser}from '../data/users.js';
+import {Router} from 'express';
+import {xss} from 'xss';
+const router = Router();
+
+router.route('/register')
+.get(async(req, res) => {
+    if (req.session.user){
+        res.redirect('/authorize');
+      } else{
+        res.render('register');
+      }
+})
+.post(async(req, res) => {
+    let userData = req.body;
+    if (!userData || Object.keys(userData).length !== 5){
+        return res.status(400).render('register', {error: "All fields need to be supplied."});
+    }
+
+
+    try{
+      userData.firstName = xss(helper.validString(userData.firstName, "First name"));
+      userData.lastName = xss(helper.validName(userData.lastName, "Last name"));
+      userData.email = xss(helper.checkEmail(email));
+      userData.username = xss(helper.checkUsername(username));
+      userData.password = xss(helper.checkPassword(password));
+    } catch(e){
+      return res.status(400).render('register', {error: e});
+    }
+
+    if(userData.password !== userData.confirmPassword){
+      return res.status(400).render('register', {error: "Passwords do not match."});
+    }
+    
+    try{
+      const newUser = await createUser(userData.firstName, userData.lastName, userData.email, userData.username, userData.password);
+      res.redirect('/login');
+      req.session.user = newUser;
+    } catch (e){
+      return res.status(400).render('register', {error: e});
+    }
+  });
+
+router.route('/')
