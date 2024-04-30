4eea681d7a634fdac6bbd636f43b1986b725ec1d
cadecermak <ccermak@stevens.edu>
2024-04-29 23:04:33 -0400

    added playlists

---
+++ data/playlists.js
@@ -0,0 +1,42 @@
+import * as c from "../config/mongoCollections.js";
+import {ObjectId} from 'mongodb';
+
+export const getAll = async () => {
+  const playlistCollection = await c.playlists();
+  const playlistList = await playlistCollection.find({}).toArray();
+  if (!playlistList) throw 'Could not get all playlists';
+  return playlistList;
+};
+
+export const getAllPosted = async () => {
+    const playlistCollection = await c.playlists();
+    const playlistList = await playlistCollection.find({posted: true}).toArray();
+    if (!playlistList) throw 'Could not get all playlists';
+    return playlistList;
+  };
+
+export const get = async (playlistID) => {
+  if (!playlistID) throw 'You must provide an id to search for';
+  if (typeof playlistID !== 'string') throw 'Id must be a string';
+  if (playlistID.trim().length === 0) throw 'id cannot be an empty string or just spaces';
+  playlistID = playlistID.trim();
+  if (!ObjectId.isValid(playlistID)) throw 'invalid object ID';
+  const playlistCollection = await c.playlists();
+  const playlist = await playlistCollection.findOne({_id: new ObjectId(playlistID)});
+  if (!playlist) throw 'No product with that id';
+  playlist._id = playlist._id.toString();
+  return playlist;
+};
+
+export const remove = async (playlistID) => {
+    if (!playlistID) throw 'You must provide an id to search for';
+    if (typeof playlistID !== 'string') throw 'Id must be a string';
+    if (playlistID.trim().length === 0) throw 'id cannot be an empty string or just spaces';
+    playlistID = playlistID.trim();
+    if (!ObjectId.isValid(playlistID)) throw 'invalid object ID';
+    const playlistCollection = await c.playlists();
+    const deletionInfo = await playlistCollection.findOneAndDelete({_id: new ObjectId(playlistID)});
+  
+    if (!deletionInfo) { throw `Could not delete product with id of ${playlistID}`;}
+    return `${deletionInfo.title} has been successfully deleted!`;
+  };
