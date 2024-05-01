30bbddfd5089fd5185415a4e2bc8f94464549715
arad15 <79551067+arad15@users.noreply.github.com>
2024-05-01 16:42:22 -0400

    Added ./ to render

--- routes/Routes.js
+++ routes/Routes.js
@@ -163,7 +163,7 @@ try{
       return res.status(400).json({error: e});
     }
     // if we get the feed, render socialFeed
-    res.render('/socialFeed', {playlists:feed, script_partial:'like_and_comment_ajax'});
+    res.render('./socialFeed', {playlists:feed, script_partial:'like_and_comment_ajax'});
   })
 
     // AJAX routes for like and comment
