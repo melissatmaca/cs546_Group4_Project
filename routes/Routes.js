409ac95e1eab3049800314e421f585b657de8a58
Bernard Vitale <126836061+Bernard-Vitale@users.noreply.github.com>
2024-05-01 14:46:57 -0400

    Combined the two routes folder

--- routes/Routes.js
+++ routes/Routes.js
@@ -162,65 +162,70 @@ try{
     res.render('/socialFeed', {playlists:feed, script_partial:'like_and_comment_ajax'});
   })
 
-  router.route('/register')
-.get(async(req, res) => {
-    if (req.session.user){
+router.route('/register')
+  .get(async(req, res) => {
+      if (req.session.user){
+          res.redirect('/authorize');
+        } else{
+          res.render('register');
+        }
+  })
+  .post(async(req, res) => {
+      let userData = req.body;
+      if (!userData || Object.keys(userData).length !== 5){
+          return res.status(400).render('register', {error: "All fields need to be supplied."});
+      }
+
+      try{
+        userData.firstName = xss(helper.validString(userData.firstName, "First name"));
+        userData.lastName = xss(helper.validName(userData.lastName, "Last name"));
+        userData.email = xss(helper.checkEmail(email));
+        userData.username = xss(helper.checkUsername(username));
+        userData.password = xss(helper.checkPassword(password));
+      } catch(e){
+        return res.status(400).render('register', {error: e});
+      }
+
+      if(userData.password !== userData.confirmPassword){
+        return res.status(400).render('register', {error: "Passwords do not match."});
+      }
+      
+      try{
+        const newUser = await createUser(userData.firstName, userData.lastName, userData.email, userData.username, userData.password);
+        res.redirect('/login');
+        req.session.user = newUser;
+      } catch (e){
+        return res.status(400).render('register', {error: e});
+      }
+    });
+
+
+    router.route('/login')
+    .get(async(req, res) => {
+      if (req.session.user){
         res.redirect('/authorize');
       } else{
-        res.render('register');
+        res.redirect('/login');
+      };
+  })
+  .post(async(req, res) => {
+      let userData = req.body;
+      if (!userData || Object.keys(userData).length !== 2){
+          return res.status(400).render('login', {error: "All fields need to be supplied."});
+      };
+
+      try{
+        userData.username = xss(helper.validUsername(userData.username));
+        userData.password = xss(helper.validPassword(userData.password));
+      } catch(e){
+        return res.status(400).render('login', {error: e});
       }
-})
-.post(async(req, res) => {
-    let userData = req.body;
-    if (!userData || Object.keys(userData).length !== 5){
-        return res.status(400).render('register', {error: "All fields need to be supplied."});
-    }
 
-    try{
-      userData.firstName = xss(helper.validString(userData.firstName, "First name"));
-      userData.lastName = xss(helper.validName(userData.lastName, "Last name"));
-      userData.email = xss(helper.checkEmail(email));
-      userData.username = xss(helper.checkUsername(username));
-      userData.password = xss(helper.checkPassword(password));
-    } catch(e){
-      return res.status(400).render('register', {error: e});
-    }
-
-    if(userData.password !== userData.confirmPassword){
-      return res.status(400).render('register', {error: "Passwords do not match."});
-    }
+      let loggedUser = undefined;
+      try {
+        loggedUser = await loginUser(userData.username, userData.password);
+      } catch(e) {
+        return res.status(400).render('login', {error: "Invalid username and/or password."});
+      }
+  })
     
-    try{
-      const newUser = await createUser(userData.firstName, userData.lastName, userData.email, userData.username, userData.password);
-      res.redirect('/login');
-      req.session.user = newUser;
-    } catch (e){
-      return res.status(400).render('register', {error: e});
-    }
-  });
-
-
-  router.route('/login')
-  .get(async(req, res) => {
-    if (req.session.user){
-      res.redirect('/authorize');
-    } else{
-      res.redirect('/login');
-    };
-})
-.post(async(req, res) => {
-  let userData = req.body;
-    if (!userData || Object.keys(userData).length !== 2){
-        return res.status(400).render('login', {error: "All fields need to be supplied."});
-    }
-
-    try{
-      userData.username = xss(helper.validUsername(userData.username));
-      userData.password = xss(helper.validPassword(userData.password));
-    } catch(e){
-      return res.status(400).render('login', {error: e});
-    }
-  
-})
-
-
